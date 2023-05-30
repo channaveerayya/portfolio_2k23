b5c61443095e25493e6b95301d628b44ec40a7bd
channaveerayya <channucs0009@gmail.com>
2023-05-30 15:23:50 +0530

    Particles created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import Particles from "components/particles"
+
 export default function Home() {
   return (
-    <main>hhh </main>
+    <main>	<Particles
+    className="absolute inset-0 -z-10 animate-fade-in"
+    quantity={100}
+  /> </main>
   )
 }
